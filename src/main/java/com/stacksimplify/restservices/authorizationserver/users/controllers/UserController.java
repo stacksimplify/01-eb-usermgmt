d74b8eca5de948e64a79fea20a21653b8ab18d37
Kalyan Reddy Daida <kdaida@Kalyans-MacBook-Pro.local>
2019-09-10 12:18:11 +0530

    V5-CICD

--- src/main/java/com/stacksimplify/restservices/authorizationserver/users/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/authorizationserver/users/controllers/UserController.java
@@ -57,7 +57,7 @@ public class UserController {
 			user.setEnabled(u.isEnabled());
 			user.setFirstname(u.getFirstname());
 			user.setLastname(u.getLastname());
-			user.setAppversion("V4-CICD");
+			user.setAppversion("V5-CICD");
 			return user;
 		}).collect(Collectors.toList());
 		return filteredUsers;
