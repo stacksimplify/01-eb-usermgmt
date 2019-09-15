8d8b239541c89272f3594317d611c3670ff7b0b2
Kalyan Reddy Daida <kdaida@Kalyans-MacBook-Pro.local>
2019-09-15 16:48:38 +0530

    H2-Commit-V2

--- src/main/java/com/stacksimplify/restservices/authorizationserver/users/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/authorizationserver/users/controllers/UserController.java
@@ -57,7 +57,7 @@ public class UserController {
 			user.setEnabled(u.isEnabled());
 			user.setFirstname(u.getFirstname());
 			user.setLastname(u.getLastname());
-			user.setAppversion("V1-network");
+			user.setAppversion("V1");
 			return user;
 		}).collect(Collectors.toList());
 		return filteredUsers;
