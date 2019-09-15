1716757e8c109e7af4b3d0c84b7b424bff3d4658
Kalyan Reddy Daida <kdaida@Kalyans-MacBook-Pro.local>
2019-09-15 16:43:10 +0530

    H2-commits

--- src/main/java/com/stacksimplify/restservices/authorizationserver/users/controllers/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/authorizationserver/users/controllers/HelloWorldController.java
@@ -8,6 +8,8 @@ import java.util.Date;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stacksimplify.restservices.authorizationserver.users.entities.SimpleMessage;
+
 @RestController
 public class HelloWorldController {
 
@@ -20,5 +22,12 @@ public class HelloWorldController {
         
 		return "Hello World-CICD-V5: "+strDate;
 	}
+	
+	
+	@GetMapping("/hello-bean")
+	public SimpleMessage helloBean() {
+		return new SimpleMessage("Hello World Bean", "V1");
+	}
+	
 	
 }
