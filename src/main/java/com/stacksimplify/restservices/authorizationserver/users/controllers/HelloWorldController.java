9a4b5e73bf03bb8621cf2e0184cec6c4a208db23
Kalyan Reddy Daida <kdaida@Kalyans-MacBook-Pro.local>
2019-09-26 14:50:35 +0530

    V1 h2 jar

--- src/main/java/com/stacksimplify/restservices/authorizationserver/users/controllers/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/authorizationserver/users/controllers/HelloWorldController.java
@@ -1,10 +1,5 @@
 package com.stacksimplify.restservices.authorizationserver.users.controllers;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +10,7 @@ public class HelloWorldController {
 
 	@GetMapping("/hello")
 	public String simpleHello() {
-		
-        Date date = Calendar.getInstance().getTime();  
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
-        String strDate = dateFormat.format(date);
-        
-		return "Hello World-CICD-V5: "+strDate;
+		return "Hello World - V1";
 	}
 	
 	
